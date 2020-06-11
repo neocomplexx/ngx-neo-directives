c3132daaca0f847341d6fac0abf65034f1f406fa
diegoanguiano <diegoanguiano@hotmail.com>
2020-06-11 16:50:10 -0300

    Actualizada directivas de Numbers y Decimals

--- projects/ngx-neo-directives-mat/src/lib/ngx-neo-directives.module.ts
+++ projects/ngx-neo-directives-mat/src/lib/ngx-neo-directives.module.ts
@@ -2,10 +2,10 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommandDirective, CommandOptions, COMMAND_CONFIG } from './ngx-command/command.directive';
 import { OnReturnDirective } from './onReturn.directive';
 import { StringsDirective } from './strings.directive';
-import { NumbersDirective } from './numbers.directive';
+import { NeoNumbersDirective } from './neo-numbers.directive';
 import { NeoAutofocusDirective } from './neo-autofocus.directive';
 import { NeoChangeCommandDirective } from './neo-changecommand.directive';
-import { NeoDecimalNumbersDirective } from './decimalNumbers.directive';
+import { NeoDecimalNumbersDirective } from './neo-decimalNumbers.directive';
 import { NeoCompositeInputDirective } from './composite-input/composite-input.directive';
 import { NeoMatchHeightDirective } from 'projects/ngx-neo-directives/src/public_api';
 
@@ -17,7 +17,7 @@ import { NeoMatchHeightDirective } from 'projects/ngx-neo-directives/src/public_
     CommandDirective,
     OnReturnDirective,
     StringsDirective,
-    NumbersDirective,
+    NeoNumbersDirective,
     NeoAutofocusDirective,
     NeoChangeCommandDirective,
     NeoDecimalNumbersDirective,
@@ -28,7 +28,7 @@ import { NeoMatchHeightDirective } from 'projects/ngx-neo-directives/src/public_
     CommandDirective,
     OnReturnDirective,
     StringsDirective,
-    NumbersDirective,
+    NeoNumbersDirective,
     NeoAutofocusDirective,
     NeoChangeCommandDirective,
     NeoDecimalNumbersDirective,
